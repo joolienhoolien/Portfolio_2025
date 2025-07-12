4707e8026dfd86008fae077b8a46be912b4484ed
joolienhoolien <julienpecquet@gmail.com>
2025-07-12 16:22:59 -0600

    Updating environment variables

--- send_email.py
+++ send_email.py
@@ -1,14 +1,15 @@
 import smtplib
 import ssl
 import os
+import streamlit as st
 
 
 def send_email(user_message):
     host = "smtp.gmail.com"
     port = 465
-    username = os.getenv("PORTFOLIO_EMAIL")
-    password = os.getenv("PORTFOLIO_PW")
-    receiver_email = os.getenv("PORTFOLIO_EMAIL")
+    username = st.secrets["PORTFOLIO_EMAIL"]
+    password = st.secrets["PORTFOLIO_PW"]
+    receiver_email = st.secrets["PORTFOLIO_EMAIL"]
     context = ssl.create_default_context()
     message = user_message
     with smtplib.SMTP_SSL(host, port, context=context) as server:
