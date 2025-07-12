9d6cd8550a295b635309875f4dcc753db597b237
joolienhoolien <julienpecquet@gmail.com>
2025-07-12 15:11:59 -0600

    Adding contact_me.py page and testing form

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ col1, col2 = st.columns([1,2],vertical_alignment="top")
 with col1:
     st.image(width=300, image="images/photo.png")
     with st.container():
-        social_col1, social_col2 = st.columns(2, gap=None)
+        social_col1, social_col2, social_col3 = st.columns(3, gap=None)
         with social_col1:
             st.link_button("Github", "https://github.com/joolienhoolien")
         with social_col2:
             st.link_button("Linkedin", "https://www.linkedin.com/in/julien-pecquet/")
+        with social_col3:
+            st.page_link(page="pages/contact_me.py", label="Email")
 
 with col2:
     st.title("Julien Pecquet")
@@ -33,35 +35,39 @@ st.markdown("## What would you like to know about me?")
 expand_professional = st.expander("Professional Experience", icon=":material/info:")
 with expand_professional:
     expand_professional.write("Inside the expander.")
-    with st.container(height=300):
-        #long_text = "Lorem ipsum. " * 1000
-        #st.markdown(long_text)
-        df = pandas.read_csv("data.csv", sep=';')
-        for index, row in df.iterrows():
-            if row["context"] == "professional":
-                st.header(row["title"])
+    df = pandas.read_csv("data.csv", sep=';')
+    for index, row in df.iterrows():
+        if row["context"] == "professional":
+            st.header(row["title"])
 
 #TODO: Add area for tech stack
 #TODO: Allow pill buttons to filter list by tech
-
+#TODO: Make the images smaller
 expand_projects = st.expander("Personal Projects", icon=":material/info:")
 with expand_projects:
     expand_projects.write("Inside the expander.")
-    with st.container(height=300):
-        #long_text = "Lorem ipsum. " * 1000
-        #st.markdown(long_text)
-        df = pandas.read_csv("data.csv", sep=';')
-        for index, row in df.iterrows():
-            if row["context"] == "personal":
-                st.header(row["title"])
+    df = pandas.read_csv("data.csv", sep=';')
+    left_col, right_col = st.columns(2)
+    for index, row in df.iterrows():
+        if row["context"] == "personal":
+            if not index % 2:
+                with left_col:
+                    st.header(row["title"])
+                    st.write(row["description"])
+                    st.image("images/" + row["image"])
+                    st.write("[Source Code]("+ row["url"] + ")")
+            else:
+                with right_col:
+                    st.header(row["title"])
+                    st.write(row["description"])
+                    st.image("images/" + row["image"])
+                    st.write("[Source Code]("+ row["url"] + ")")
+
 
 expand_education = st.expander("Education & Certificates", icon=":material/info:")
 with expand_education:
     expand_education.write("Inside the expander.")
-    with st.container(height=300):
-        #long_text = "Lorem ipsum. " * 1000
-        #st.markdown(long_text)
-        df = pandas.read_csv("data.csv", sep=';')
-        for index, row in df.iterrows():
-            if row["context"] == "education":
-                st.header(row["title"])
+    df = pandas.read_csv("data.csv", sep=';')
+    for index, row in df.iterrows():
+        if row["context"] == "education":
+            st.header(row["title"])
